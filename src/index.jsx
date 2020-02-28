8dafc5bef9d63e36ba3c9d20ca35484632040d11
Henry Le Grys <henry@legrys.info>
2020-02-28 10:08:50 +0000

    Change player & background to persist through routing changes

--- src/index.jsx
+++ src/index.jsx
@@ -17,36 +17,27 @@ import playerReducer from "./ducks/player";
 
 class Homepage {
     view() {
-        return <div class="container">
-            <BackgroundPlate/>
-            <Navbar/>
-            <Player/>
-            <div class="block">
-                <Splash/>
-                <h1>Podcasts</h1>
-                <PodcastGallery/>
-            </div>
+        return <div class="block">
+            <Splash/>
+            <h1>Podcasts</h1>
+            <PodcastGallery/>
         </div>
     }
 }
 
 class AboutPage {
     view() {
-        return <div class="container">
-            <BackgroundPlate/>
-            <Navbar/>
-            <div class="block filled">
-                <p>
-                    76 Podcasting is run by the same team that runs University Radio Falmer
-                    <br/>
-                    My money's in that office, right? If she start giving me some bullshit about it ain't there,
-                    and we got to go someplace else and get it, I'm gonna shoot you in the head then and there.
-                    Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is.
-                    She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen:
-                    we go in there, and that dude Winston or anybody else is in there, you the first motherfucker
-                    to get shot. You understand?
-                </p>
-            </div>
+        return <div class="block filled">
+            <p>
+                76 Podcasting is run by the same team that runs University Radio Falmer
+                <br/>
+                My money's in that office, right? If she start giving me some bullshit about it ain't there,
+                and we got to go someplace else and get it, I'm gonna shoot you in the head then and there.
+                Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is.
+                She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen:
+                we go in there, and that dude Winston or anybody else is in there, you the first motherfucker
+                to get shot. You understand?
+            </p>
         </div>
     }
 }
@@ -59,13 +50,8 @@ function PodcastPage(vnode) {
     return {
         oninit: run,
         view(vnode) {
-            return <div class="container">
-                <BackgroundPlate/>
-                <Navbar/>
-                <Player/>
-                <div class="block">
-                    {podcast() ? m(Podcast, podcast()) : <h1>Loading...</h1>}
-                </div>
+            return <div class="block">
+                {podcast() ? m(Podcast, podcast()) : <h1>Loading...</h1>}
             </div>
         }
     }
@@ -73,8 +59,24 @@ function PodcastPage(vnode) {
 
 createStore({ player: playerReducer });
 
+function RootResolver(WrappedComponent) {
+    return {
+        onmatch() {
+            return WrappedComponent;
+        },
+        render(vnode) {
+            return <div class="container">
+                <BackgroundPlate/>
+                <Navbar/>
+                <Player/>
+                {vnode}
+            </div>
+        }
+    };
+}
+
 m.route(document.body, "/home", {
-    "/home": Homepage,
-    "/about": AboutPage,
-    "/podcast/:slug": PodcastPage,
+    "/home": RootResolver(Homepage),
+    "/about": RootResolver(AboutPage),
+    "/podcast/:slug": RootResolver(PodcastPage),
 });
