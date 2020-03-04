dcb52baeefb817f3f9c06dce41f471e3f4dc0958
Henry <henry@legrys.info>
2020-03-04 17:31:04 +0000

    Change contact page styling to be more consistent

--- src/index.jsx
+++ src/index.jsx
@@ -29,15 +29,18 @@ class Homepage {
 
 class ContactPage {
     view() {
-        return <div class="block filled contact-page">
-            <p>
-                To apply for a show or get involved,
-                email <a class="stylish" href="mailto:podcasting@urfonline.com">podcasting@urfonline.com</a>.
-            </p>
-            <p>
-                Powered by URF at <a class="stylish urf-red" href="https://urfonline.com">urfonline.com</a>
-            </p>
-            <SocialIcons/>
+        return <div class="block">
+            <h1>Contact</h1>
+            <div class="contact-page">
+                <p>
+                    To apply for a show or get involved,
+                    email <a class="stylish" href="mailto:podcasting@urfonline.com">podcasting@urfonline.com</a>.
+                </p>
+                <p>
+                    Powered by URF at <a class="stylish urf-red" href="https://urfonline.com">urfonline.com</a>
+                </p>
+                <SocialIcons/>
+            </div>
         </div>
     }
 }
