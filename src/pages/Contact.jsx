49df472248b03e64a491b2fd3c62af3bd1296796
Henry Le Grys <henry@legrys.info>
2020-10-01 02:04:35 +0100

    Query the API for contact page custom content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,34 @@
 import m from "mithril";
 import SocialIcons from "../components/SocialIcons";
+import {useQuery} from "../util/QueryClient";
+import QPages from "../queries/pages.gql";
 
-export default class ContactPage {
-    view() {
-        return <div class="block">
-            <h1>Contact</h1>
-            <div class="contact-page">
-                <p>
-                    To apply for a show or get involved,
-                    email <a class="stylish" href="mailto:podcasting@urfonline.com">podcasting@urfonline.com</a>.
-                </p>
-                <p>
-                    Powered by URF at <a class="stylish urf-red" href="https://urfonline.com">urfonline.com</a>
-                </p>
-                <SocialIcons/>
+export default function ContactPage(vnode) {
+    let [result, run, error] = useQuery(QPages, { slug: "contact" });
+
+    let pageInfo = result.map(data => data.podcastPage);
+
+    return {
+        oninit: run,
+        view() {
+            if (!pageInfo()) {
+                return <div class="block">
+                    {error() ? <div class="error">Couldn't load that page :(</div> : <h1>Loading...</h1>}
+                </div>
+            }
+
+            return <div class="block">
+                <h1>{pageInfo().title}</h1>
+                <div class="custom-page">
+                    <div class="custom-content">
+                        {m.trust(pageInfo().body)}
+                        <p>
+                            Powered by URF at <a class="urf-red" href="https://urfonline.com">urfonline.com</a>
+                        </p>
+                    </div>
+                    <SocialIcons/>
+                </div>
             </div>
-        </div>
+        }
     }
 }
